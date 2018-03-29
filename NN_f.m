d476bdd04de0940bc197cdeba1ef8f10ca86ea11
dekaramich <37862653+dekaramich@users.noreply.github.com>
2018-03-30 00:11:37 +0300

    main

--- NN_f.m
+++ NN_f.m
@@ -1,5 +1,7 @@
 clear
+%% input
 %dataset: 3_autoMPG, Fuzzy Means (s = 10)
+%centers
 load 'centers.mat' 'c'
 %training dataset
 load 'xtrnorm.mat' 'xtrnorm'
@@ -8,39 +10,39 @@ load 'ytrnorm.mat' 'ytrnorm'
 %N: no. of centers %no_var: no. of variables %data: no. of training examples
 N = size(c,1); no_var = size(c,2); data = size(xtrnorm,1);
 
-%initial value
+%% initial value of x
+%N x no_var
 x0 = c;
+%N*no_var x 1
 x0 = vec(x0');
 
-%objective
+%% objective
 f=neural_net(xtrnorm,ytrnorm,N,data,no_var);
 call_f = f.makef();
 %initial cost, grad
-[f_x, grad_f_x] = call_f(x0);
+[f_x0, grad_f_x0] = call_f(x0);
 
-g = zeroFunction();%l1Norm(0);
+g = zeroFunction();
 
-%options
+%% options
 opt.maxit = 10;
-opt.method = 'lbfgs';
+%opt.method = 'lbfgs';
 opt.linesearch = 'lemarechal';
 
+%% results
 fprintf('\nL-BFGS\n');
-
-%results
 out_lbfgs = forbes(f, g, x0,  [], [], opt);
 fprintf('\n');
 fprintf('message    : %s\n', out_lbfgs.message);
 fprintf('iterations : %d\n', out_lbfgs.solver.iterations);
-%fprintf('matvecs    : %d\n', out_lbfgs.solver.operations.C1);
-%fprintf('prox       : %d\n', out_lbfgs.solver.operations.proxg);
 fprintf('time       : %7.4e\n', out_lbfgs.solver.ts(end));
-%fprintf('residual   : %7.4e\n', out_lbfgs.solver.residual(end));
 
-%final value of x
+%% final value of x
 finalc = out_lbfgs.x;
 
+%N x no_var
 finalc = vec2mat(finalc,no_var);
-[phi,w] = call_phi_fun(finalc,xtrnorm,N, data, no_var,ytrnorm);
 
-h = 0.5*norm(phi*w-ytrnorm)^2
+% [phi,w] = call_phi_fun(finalc,xtrnorm,N, data, no_var,ytrnorm);
+% 
+% h = 0.5*norm(phi*w-ytrnorm)^2
