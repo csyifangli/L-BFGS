85b1b6cdf1a83c603785726746902fe027b0d770
dekaramich <37862653+dekaramich@users.noreply.github.com>
2018-03-30 00:15:05 +0300

    function

--- call_phi_fun.m
+++ call_phi_fun.m
@@ -1,9 +1,11 @@
-function [phi,w] = call_phi_fun(c,xtrnorm,N, data, no_var,ytrnorm)
-    for i = 1:data       
-       for j = 1:N 
-        phi(i,j) = norm(c(j,:)-xtrnorm(i,:))^2*log10(norm(c(j,:)-xtrnorm(i,:))+1);
-       end
-    end  
-      phi(:,N+1) = 1; 
-      w = phi\ytrnorm;
-end
+% function [phi,w] = call_phi_fun(cnorm,xnorm,N, data, no_var,ytrnorm,sigma)
+%  phi(1:data,1:N+1) = 0;
+%     for i = 1:data
+%         for j = 1:N
+%             phi(i,j) = exp(-norm(c(j,:)-xtrnorm(i,:))^2/(2*sigma(1,j)^2));
+%         end
+%     end
+%     phi(:,N+1) = 1; 
+%     w = phi\ytrnorm;
+% end
+%     
